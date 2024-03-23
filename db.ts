c541a01b9da9e3c8b2481a3613e2e6c6a520b78c
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-03-23 07:39:58 +0530

    moved db.ts to /

--- db.ts
+++ db.ts
@@ -2,11 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const connectionString = process.env.DATABASE_URL
-console.log(connectionString);
 if (!connectionString) {
     throw new Error('DATABASE_URL is not set')
 }
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString)
+export const client = postgres(connectionString, { prepare: true, debug: true })
 export const db = drizzle(client);
